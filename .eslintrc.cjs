c8c2de67ae2c9ce77944e3835ad588c78cbdd750
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-20 13:27:38 +0900

    yarn add -D eslint eslint-plugin-vue typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser @nuxtjs/eslint-config-typescript eslint-plugin-tailwindcss

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    '@nuxtjs/eslint-config-typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'tailwindcss'],
+  rules: {
+    semi: 'off',
+    'comma-dangle': 'off',
+    'tailwindcss/no-custom-classname': [
+      'warn',
+      {
+        config: 'tailwind.config.cjs',
+      },
+    ],
+  },
+};
