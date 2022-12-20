d85754514fde58246ca174a9825124e8057070b5
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-20 14:36:28 +0900

    yarn add daisyui

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,5 +3,6 @@
 <template>
   <div class="p-3">
     <h2>Successful!</h2>
+    <button class="btn">Button</button>
   </div>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,4 +8,6 @@ module.exports = {
       },
     },
   },
+  plugins: [require('daisyui')],
+  daisyui: {},
 };
