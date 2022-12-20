eab789add4c4c1e33d10fe4aab66d57c5777ca4d
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-20 11:58:35 +0900

    Set nitro, devServer, srcDir in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  nitro: {
+    preset: 'node',
+  },
+  devServer: {
+    host: '0.0.0.0',
+  },
+  srcDir: 'src',
+});
